0686f49bc4314e7513e04f74a79b1b3266a1e7b4
UlianaGritchina <ugritchina@icloud.com>
2022-02-24 13:46:13 +0300

    Initial Commit

---
+++ Animaton/AnimatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatonApp.swift
+//  Animaton
+//
+//  Created by Ульяна Гритчина on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animaton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animaton
+//
+//  Created by Ульяна Гритчина on 24.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
